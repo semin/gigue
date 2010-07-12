db65f1c7f9dca553a116ac34783b3633a34f63dd
Semin Lee <seminlee@gmail.com>
2010-07-12 10:08:01 +0100

    First commit

---
+++ test/test_gigue_cli.rb
@@ -0,0 +1,14 @@
+require File.join(File.dirname(__FILE__), "test_helper.rb")
+require 'gigue/cli'
+
+class TestGigueCli < Test::Unit::TestCase
+  def setup
+    Gigue::CLI.execute(@stdout_io = StringIO.new, [])
+    @stdout_io.rewind
+    @stdout = @stdout_io.read
+  end
+  
+  def test_print_default_output
+    assert_match(/To update this executable/, @stdout)
+  end
+end
