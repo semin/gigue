14810baaf961df84d2a94598c107a5b97032dc02
Semin Lee <seminlee@gmail.com>
2010-07-12 17:42:18 +0100

    Implemented profile sequence global alignment

--- lib/gigue.rb
+++ lib/gigue.rb
@@ -1,6 +1,14 @@
 $:.unshift(File.dirname(__FILE__)) unless
   $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
 
+require 'narray'
+require File.join(File.dirname(__FILE__), 'enumerable_extensions')
+require File.join(File.dirname(__FILE__), 'gigue', 'profile')
+require File.join(File.dirname(__FILE__), 'gigue', 'profile_position')
+require File.join(File.dirname(__FILE__), 'gigue', 'profile_sequence_aligner')
+require File.join(File.dirname(__FILE__), 'gigue', 'profile_sequence_alignment')
+
 module Gigue
   VERSION = '0.0.1'
+  NONE, UP, LEFT, DIAG = 0, 1, 2, 3
 end
